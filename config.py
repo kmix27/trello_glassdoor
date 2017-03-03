bf31c02a38e08eafc4ec2f675fffc2206226b873
kyle mix <kylemix@kyles-MacBook-Pro.local>
2017-03-02 16:59:08 -0800

    first commit

---
+++ config.py
@@ -0,0 +1,32 @@
+import trello
+from trello import TrelloClient
+import json
+import argparse 
+
+
+
+def main(key, token):
+    client = TrelloClient(api_key=key, token=token)
+    new_board = client.add_board('Job Search Board')
+    for i in new_board.all_lists():
+        i.close()
+    links_label = new_board.add_label(name='Links',color='sky')
+    desc_label = new_board.add_label(name='Description', color='purple')
+    checklist_label = new_board.add_label(name='To Do', color='red')
+    config = dict(board_id=new_board.id,
+        link_l=links_label.id, 
+        desc_l=desc_label.id, 
+        check_l=checklist_label.id,
+        key=key,
+        token=token)
+    with open('config.json','w') as c:
+        json.dump(config,c)
+        print('Successfully Created config file\n')
+        print('New board at https://trello.com/b/{}'.format(config['board_id']))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('key',type=str, help='add your trello api key')
+parser.add_argument('token',type=str, help='add your trello api token')
+args = parser.parse_args()
+
+main(key=args.key, token=args.token)
---
+++ trellogd.py
@@ -0,0 +1,87 @@
+import requests
+import lxml
+from bs4 import BeautifulSoup
+try:
+    import trello
+    from trello import TrelloClient
+except(ImportError):
+    print('Please run: \npip install py-trello')
+    exit()
+try:
+    from fake_useragent import UserAgent
+except(ImportError):
+    print('Please run: \npip install fake-useragent')
+    exit()
+from datetime import time, datetime, timedelta
+import argparse
+import json
+import html2text
+
+with open('config.json','r') as c:
+    auth = json.load(c)
+
+
+
+def pull_data(url):
+    ua = UserAgent()
+    head = ua.random
+    resp = requests.get(url, headers={'User-Agent':head})
+    soup = BeautifulSoup(resp.text,'lxml')
+    hcell = soup.find(class_='header cell info')
+    job_title = hcell.find(class_='noMargTop margBotSm strong')
+    job = job_title.text
+    dept = ''
+    if len(job.split(', ')) > 1:
+        dept = job.split(',')[-1].strip()
+    if len(job.split('-')) > 1:
+        dept = job.split('-')[-1].strip()
+    company = hcell.find(class_='ib').text.strip()
+    city = hcell.find(class_='subtle ib').text.replace(' – ','').split(',')[0]
+    url = soup.find(class_='empLinks tbl').find('a')['href']
+    ap_type = soup.find(class_='empLinks tbl').find('a').text
+    descHtml = soup.find(class_='jobDescriptionContent desc')
+    h = html2text.HTML2Text()
+    desc = h.handle(str(descHtml))
+    data = dict(job=job,url=url,ap_type=ap_type,desc=desc,descHtml=descHtml, company=company, city=city, dept=dept)
+    return data
+
+def populate_board(url):
+    client = TrelloClient(
+        api_key=auth['key'],
+        token=auth['token'])
+    board = client.get_board(board_id=auth['board_id'])
+    link_l = client.get_label(label_id= auth['link_l'], board_id=auth['board_id'])
+    desc_l = client.get_label(label_id= auth['desc_l'], board_id=auth['board_id'])
+    check_l = client.get_label(label_id= auth['check_l'], board_id=auth['board_id'])
+    data = pull_data(url)
+    data['gd_url'] = url
+    list_name = data['company'] + ':\n' + data['job'] + '\n\nAdded: ' + str(datetime.now())
+    j_title = data['company'] + ': ' + data['job']
+    new_list = board.add_list(name=list_name,pos=None)
+    description = new_list.add_card(name='{} \nDetails:'.format(j_title), desc=data['desc'])
+    description.add_label(desc_l)
+    attachCard = new_list.add_card(name='{} \nLinks:'.format(j_title))
+    attachCard.attach(name=data['ap_type'],url=data['url'])
+    attachCard.attach(name='GlassDoor posting',url=data['gd_url'])
+    if data['dept'] != '':
+        attachCard.attach(name='Linkedin search, Company + Department',
+            url='https://www.linkedin.com/search/results/people/?keywords=' + data['company'].replace(' ','%20') + '%20' + data['dept'].replace(' ', '%20'))
+    else:
+        attachCard.attach(name='Linkedin search, Company',
+            url='https://www.linkedin.com/search/results/people/?keywords=' + data['company'].replace(' ', '%20'))
+    attachCard.add_label(link_l)
+    dt = datetime.now() + timedelta(days=3)
+    cl = ['Research Hiring manager', 'Polish Resume', 
+          'Craft Cover Letter', 'Attempt to make contact on personal level',
+          'Complete application', 'Send application',
+          'Wait three days: {}'.format(str(dt.date())), 'Send follow up email']
+    clCard = new_list.add_card('To Do List')
+    clCard.add_checklist(title='To Do!\n\n{}'.format(j_title), items=cl)
+    clCard.add_label(check_l)
+    print('\n' + 'Success! \n\nAdded {} to you board'.format(j_title) + '\n\nCheck it out here:\nhttps://trello.com/b/{}'.format(auth['board_id'] + '\n'))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('url',type=str, help='add a glassdoor job url to your trello board')
+args = parser.parse_args()
+
+populate_board(args.url)
